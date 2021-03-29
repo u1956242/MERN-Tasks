f92d4d3c20cc74c9df72a44d98dee55a2ac4c19c
Iuri <u1956242@campus.udg.edu>
2021-03-29 12:15:15 +0200

    First commit

---
+++ routes/auth.js
@@ -0,0 +1,24 @@
+// Rutas para autenticar usuarios
+const express = require('express');
+const router = express.Router();
+const { check } = require('express-validator');
+const authController = require('../controllers/authController');
+const auth = require('../middleware/auth');
+
+// Iniciar Sesión
+// api/auth
+router.post('/', 
+    [
+        check('email', 'Agrega un email válido').isEmail(),
+        check('password', 'El password debe ser minimo de 6 caracteres').isLength({ min: 6 })
+    ],
+    authController.autenticarUsuario
+);
+
+// Obtiene el usuario autenticado
+router.get('/',
+    auth,
+    authController.usuarioAutenticado
+);
+
+module.exports = router;
