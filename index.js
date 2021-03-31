8b93456e9b84e8df3335f289f5d586113cd1964d
Iuri <u1956242@campus.udg.edu>
2021-03-31 19:10:42 +0200

    Port

--- index.js
+++ index.js
@@ -25,5 +25,5 @@ app.use('/api/tareas', require('./routes/tareas'));
 
 // Arrancar la app
 app.listen(port, '0.0.0.0', () => {
-    console.log(`El servidor esta funcionando en el puerto ${PORT}`);
+    console.log(`El servidor esta funcionando en el puerto ${port}`);
 });
